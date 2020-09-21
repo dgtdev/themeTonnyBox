b190777a50682ab3211eca4b0e9aa9da08a25721
dgtdev <d_avisote@hotmail.com>
2020-09-21 17:03:19 -0500

    contenedor

--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
 	
 		<footer class="site-footer contenedor">
-
-			<div class="contenido-footer">
-				
-				<p class="copyrigth"> Todos los derechos reservados. <?php echo get_bloginfo('name') . " " . date('Y') ?> </p>
-			</div>		
+			<div class="contenedor">
+				<div class="contenido-footer">
+					
+					<p class="copyrigth"> Todos los derechos reservados. <?php echo get_bloginfo('name') . " " . date('Y') ?> </p>
+				</div>
+			</div>
 		</footer>
 
 		<?php wp_footer(); ?>
