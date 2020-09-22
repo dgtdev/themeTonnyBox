792ca8432d611d700df7b346e2c6664bfa0b8e28
dgtdev <d_avisote@hotmail.com>
2020-09-21 20:01:39 -0500

    footer v1

--- footer.php
+++ footer.php
@@ -1,10 +1,22 @@
 	
-		<footer class="site-footer contenedor">
+		<footer class="site-footer">
 			<div class="contenedor">
-				<div class="contenido-footer">
+
+				<div class="contenedor-menu"></div>
+				<div class="contenedor-menu"></div>
+				<div class="contenedor-menu"></div>
+
+				<div class="copyrigth-footer">
 					
-					<p class="copyrigth"> Todos los derechos reservados. <?php echo get_bloginfo('name') . " " . date('Y') ?> </p>
+					<p>Copyrigth © <?php  echo date('Y') ?> </p>
+
+					<p class="copyrigth"> Todos los derechos reservados para <?php echo get_bloginfo('name') ?> 
+					<span class="copyrigth">desarrollado por</span>
+					<a class="copyrigth" href="https://www.tonnybox.com" target="_blank" rel="nofollow">DGT</a>
+					</p>
+					
 				</div>
+
 			</div>
 		</footer>
 
