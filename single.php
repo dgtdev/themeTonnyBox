c86f5d96077149e85966ee60b8f3548f91f80f55
dgtdev <d_avisote@hotmail.com>
2021-02-28 01:14:48 -0600

    css

--- single.php
+++ single.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+<div class="container">
 <?php while( have_posts() ):  the_post(); ?>
 	
 	<h2><?php the_title(); ?></h2>
@@ -7,5 +8,6 @@
 	<?php the_content(); ?>
 
 <?php endwhile; ?>
+</div>
 
 <?php get_footer(); ?>
